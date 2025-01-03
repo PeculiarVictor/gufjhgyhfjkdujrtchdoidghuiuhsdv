25dee7aae518233b26735da92619581294a7e317
PeculiarVictor <160975802+PeculiarVictor@users.noreply.github.com>
2025-01-03 18:42:41 +0000

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,21 @@
+#Print a Sentence
+print("Welcome to the world of programming !")
+
+#Print a Number
+print(4)
+
+
+#Next Line after print
+print("Hello Python /n")
+
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of print Statement
+print("welcome to ", end= "*")
+# Creating different variables
+x=5
+y="John"
+print(x)
+print(y)
